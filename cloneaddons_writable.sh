6a067a081c7c688fcf0ddc11d884bd52663f6f66
obviousjim <james@jamesgeorge.org>
2012-04-07 19:19:39 -0400

    added writable project script download

---
+++ cloneaddons_writable.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ../../addons
+
+git clone git@github.com:Flightphase/ofxGameCamera
+git clone git@github.com:Flightphase/ofxRange
+git clone git@github.com:Flightphase/ofxTimeline
+git clone git@github.com:Flightphase/ofxTween
+git clone git@github.com:Flightphase/ofxMSAInteractiveObject
+git clone git@github.com:Flightphase/ofxTextInputField
+git clone git@github.com:Flightphase/ofxSimpleGuiToo
+git clone git@github.com:obviousjim/ofxRGBDepth
+git clone git@github.com:obviousjim/ofxCv
+git clone git@github.com:obviousjim/ofxCameraTrack
+
+cd ofxCv/
+git checkout opencv-2.3
+cd ..
+
+# you also need to replace ofxOpenCv/libs/opencv with this link:
+# https://github.com/kylemcdonald/ofxCv/downloads
+# opencv-2.3-osx.zip — OpenCV 2.3 library compiled on OSX 10.6.
