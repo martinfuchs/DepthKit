12b86144a3db2d6b24cce320d39149b9dbf50b89
Greg Borenstein <greg@mfdz.com>
2012-04-07 16:47:14 -0400

    bring over cloneaddons.sh from master, point it at the git@github urls

---
+++ cloneaddons.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#make sure you are in the RGBDToolkit project when you run this script
+cd ../../addons
+git clone git@github.com:obviousjim/ofxCameraTrack
+git clone git@github.com:Flightphase/ofxGameCamera
+git clone git@github.com:Flightphase/ofxRange
+git clone git@github.com:Flightphase/ofxTimeline
+git clone git@github.com:Flightphase/ofxTween
+git clone git@github.com:Flightphase/ofxMSAInteractiveObject
+git clone git@github.com:Flightphase/ofxTextInputField
+git clone git@github.com:Flightphase/ofxSimpleGuiToo
+git clone git@github.com:obviousjim/ofxRGBDepth
+git clone git@github.com:kylemcdonald/ofxCv
+cd ofxCv/
+git checkout opencv-2.3
+cd ..
+
+# you also need to replace ofxOpenCv/libs/opencv with this link:
+# https://github.com/kylemcdonald/ofxCv/downloads
+# opencv-2.3-osx.zip — OpenCV 2.3 library compiled on OSX 10.6.
